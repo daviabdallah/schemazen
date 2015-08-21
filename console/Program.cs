56f494e451256ccac0d0966774c1582d0e9aea4c
Davi Abdallah <davi.abdallah@logicpathways.com>
2015-08-21 11:34:31 -0400

    Update Program.cs
    Add action JobScript

--- console/Program.cs
+++ console/Program.cs
@@ -1,30 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using ManyConsole;
 
 namespace console {
 	internal class Program {
+
 		private static int Main(string[] args) {
 			try {
 				return ConsoleCommandDispatcher.DispatchCommand(
-					GetCommands(), args, Console.Out);
+				  GetCommands(), args, Console.Out);
 			} catch (Exception ex) {
 				Console.WriteLine(ex.Message);
 				Console.WriteLine(ex.StackTrace);
-#if DEBUG
-				if (Debugger.IsAttached)
-					ConsoleQuestion.WaitForKeyPress();
-#endif
 				return -1;
 			}
 		}
 
-		private static IEnumerable<ConsoleCommand> GetCommands() {
-			return new List<ConsoleCommand> {
-				new Script(),
-				new Create(),
-				new Compare()
+		static IEnumerable<ConsoleCommand> GetCommands() {
+			return new List<ConsoleCommand>() {
+				new Script(), new Create(), new Compare(), new JobScript()
 			};
 		}
 	}
